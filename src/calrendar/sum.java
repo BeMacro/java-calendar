d8eb138ebd4e7a1ad48c70953dc4b7e22772162e
더불어숲 <@192.168.1.104>
2018-06-25 17:05:18 +0900

    sum

--- src/calrendar/sum.java
+++ src/calrendar/sum.java
@@ -1,20 +1,20 @@
 package calrendar;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class sum {
 
 	public static void main(String[] args) {
-		int i,j;
+		int i, j;
 		System.out.println("두 수를 입력하세요");
 		Scanner sc = new Scanner(System.in);
 		i = sc.nextInt();
 		j = sc.nextInt();
-		System.out.println(i +" , "+j);
-		//System.out.println("두수의 합은 " + (i+j) + " 입니다");
-		System.out.printf("%d와 %d의 합은 %d 입니다",i, j, i+j);
+		System.out.println(i + " , " + j);
+		// System.out.println("두수의 합은 " + (i+j) + " 입니다");
+		System.out.printf("%d와 %d의 합은 %d 입니다", i, j, i + j);
 		sc.close();
-		
+
 	}
 
 }
